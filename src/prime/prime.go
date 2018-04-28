44f945ccc8e90e927d645476f153789da90dbbdc
Ralf Pannemans <ralf.pannemans@googlemail.com>
2018-04-28 12:53:54 +0200

    Add isPrime implementation

--- src/prime/prime.go
+++ src/prime/prime.go
@@ -1,5 +1,31 @@
 package prime
 
+import "math"
+
+//IsPrime checks if it is a Prime
 func IsPrime(number int) bool {
-	return false
+	if number < 2 {
+		return false
+	} else if number == 2 {
+		return true
+	} else if isDividableBy(number, 2) {
+		return false
+	}
+
+	maxDivider := calculateMaxPossibleDivider(number)
+	for ii := 3; ii <= maxDivider; ii = ii + 2 {
+		dividerFound := isDividableBy(number, ii)
+		if dividerFound {
+			return false
+		}
+	}
+	return true
+}
+
+func calculateMaxPossibleDivider(number int) int {
+	return int(math.Floor(math.Sqrt(float64(number))))
+}
+
+func isDividableBy(number int, divider int) bool {
+	return (number % divider) == 0
 }
--- src/prime/prime_test.go
+++ src/prime/prime_test.go
@@ -6,6 +6,34 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSomething(t *testing.T) {
-	assert.Equal(t, false, IsPrime(3))
+func Test2(t *testing.T) {
+	assert.Equal(t, true, IsPrime(2))
+}
+
+func Test1(t *testing.T) {
+	assert.Equal(t, false, IsPrime(1))
+}
+
+func Test0(t *testing.T) {
+	assert.Equal(t, false, IsPrime(0))
+}
+
+func Test4(t *testing.T) {
+	assert.Equal(t, false, IsPrime(4))
+}
+
+func Test3(t *testing.T) {
+	assert.Equal(t, true, IsPrime(3))
+}
+
+func Test9(t *testing.T) {
+	assert.Equal(t, false, IsPrime(9))
+}
+
+func Test113(t *testing.T) {
+	assert.Equal(t, true, IsPrime(113))
+}
+
+func Test169(t *testing.T) {
+	assert.Equal(t, false, IsPrime(169))
 }
