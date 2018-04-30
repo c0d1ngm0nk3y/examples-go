243109331996a4cbb471ad47318955f428212077
Ralf Pannemans <ralf.pannemans@googlemail.com>
2018-04-30 22:11:04 +0200

    Add roman example to main

--- src/main/hello.go
+++ src/main/hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"prime"
+	"roman"
 )
 
 func main() {
@@ -10,7 +11,8 @@ func main() {
 	fmt.Printf("hello world %d\n", x)
 	fmt.Printf("113 is prime: %v\n", prime.IsPrime(113))
 	factors := prime.GetPrimeFactors(60)
-	fmt.Printf("Factors of 60: %v (%d)", factors, len(factors))
+	fmt.Printf("Factors of 60: %v (%d)\n", factors, len(factors))
+	fmt.Printf("MLIX -> %d", roman.ToArabic("MLIX"))
 }
 
 //Sum is guessing what?
